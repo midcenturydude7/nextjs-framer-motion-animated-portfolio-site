d0489b2e5e25141a5f2f4b0e8aa214a48c3f605b
midCenturyDude7 <mgriffes@gmail.com>
2025-08-25 20:34:31 -0700

    Set up scaffolding for Header via Navbar and MobileNav components

---
+++ app/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+import MobileNav from "./MobileNav/MobileNav";
+
+export default function Header() {
+  const [focused, setFocused] = React.useState(null);
+
+  return (
+    <header className="flex border-b-[1px] border-slate-300/10 bg-[rgba(0,2,8,0,0.61)] p-8 shadow-2xl shadow-black/80">
+      <Navbar focused={focused} setFocused={setFocused} />
+      <MobileNav focused={focused} setFocused={setFocused} />
+    </header>
+  );
+}
---
+++ app/components/Header/MobileNav/MobileNav.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { motion, useCycle, AnimatePresence } from "framer-motion";
+
+export default function MobileNav({ focused, setFocused }) {
+  return <div>MobileNav</div>;
+}
