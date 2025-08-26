f8eee1b254164089dfb79eec8a9484388e033bcc
midCenturyDude7 <mgriffes@gmail.com>
2025-08-25 21:07:12 -0700

    Setting up lib folder to pull through

--- app/components/Header/MobileNav/MobileNav.jsx
+++ app/components/Header/MobileNav/MobileNav.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import Link from "next/link";
 import { motion, useCycle, AnimatePresence } from "framer-motion";
+import { cn } from "../../../lib/utils";
+import { mobileNavItems } from "../../../lib/mobileNavItems";
 
 export default function MobileNav({ focused, setFocused }) {
   return <div>MobileNav</div>;
