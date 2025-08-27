ce94f6018c403a1506af7fd91977c6805faf5f0d
midCenturyDude7 <mgriffes@gmail.com>
2025-08-26 20:45:35 -0700

    Added styles

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,9 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="https://use.typekit.net/rgm1dlw.css" />
       </head>
       <body className="set font-[sofia-pro,_sans-serif] font-medium not-italic text-slate-100/70 antialiased">
-        {children}
+        <div className="flex min-h-screen flex-col space-y-4 bg-gradient-to-b from-[rgba(12,12,39,0.83)] to-[rgba(0,2,8,0.5)]">
+          {children}
+        </div>
       </body>
     </html>
   );
