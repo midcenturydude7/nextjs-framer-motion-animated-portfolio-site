ea4e2dd1697bc7113a8193427de6c756c7014a45
midCenturyDude7 <mgriffes@gmail.com>
2025-08-28 21:21:39 -0700

    Configuring responsive layout

--- app/components/Header/MobileNav/MobileNav.jsx
+++ app/components/Header/MobileNav/MobileNav.jsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 import { motion, useCycle, AnimatePresence } from "framer-motion";
 import { cn } from "../../../lib/utils";
 import { mobileNavItems } from "../../../lib/mobileNavItems";
+import { useNavContext } from "../../../contexts/NavContext";
 
 export default function MobileNav({ focused, setFocused }) {
-  return <div>MobileNav</div>;
+  const { selectedTab, setSelectedTab } = useNavContext();
+  const [mobileNavBar, toggleMobileNavBar] = useCycle(false, true);
+
+  return <nav className="ml-auto px-8 py-4 lg:hidden">MobileNav</nav>;
 }
