0cb845c3685883296bde96b28b30925a67d901fc
midCenturyDude7 <mgriffes@gmail.com>
2025-09-01 20:43:41 -0700

    Adding both Navbar and MobileNav functionality

--- app/components/Header/Navbar/Navbar.jsx
+++ app/components/Header/Navbar/Navbar.jsx
@@ -1,5 +1,18 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { mobileNavItems } from "../../../lib/mobileNavItems";
+import { cn } from "../../../lib/utils";
+import { useNavContext } from "../../../contexts/NavContext";
 
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <nav className="nav-container lg:flex lg:flex-grow lg:justify-center">
+      <ul
+        onPointerLeave={() => setFocused(null)}
+        className="nav-wrapper space-x-4 border-slate-300/10 bg-gradient-to-b from-[rgba(21,24,43,0.75)] to-[rgba(0,0,0,0.17)] py-[0.5rem] pl-6 pr-[1.8rem]"
+      ></ul>
+    </nav>
+  );
 }
