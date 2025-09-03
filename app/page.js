7d8967ce0a348d8056cde0b7b1d65af0e7b4155c
midCenturyDude7 <mgriffes@gmail.com>
2025-09-02 20:58:05 -0700

    Updated dependencies

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
       <h1 className="text-3xl font-bold text-slate-300/70">
-        Portfolio Site - Built with Nextjs/React and animated with Framer-Motion
+        Framer Motion - Animated Tabs Exercise (Review)
       </h1>
-      <h2 className="text-transform: uppercase">home page</h2>
+      <h2>HOME PAGE</h2>
     </div>
   );
 }
