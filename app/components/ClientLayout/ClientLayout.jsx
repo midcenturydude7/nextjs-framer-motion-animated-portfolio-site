40e87060de19b425d2b465da2d793b82e14860b4
midCenturyDude7 <mgriffes@gmail.com>
2025-09-03 21:07:13 -0700

    Added comment

--- app/components/ClientLayout/ClientLayout.jsx
+++ app/components/ClientLayout/ClientLayout.jsx
@@ -4,3 +4,6 @@ import { NavProvider } from "../../contexts/NavContext";
 export default function ClientLayout({ children }) {
   return <NavProvider>{children}</NavProvider>;
 }
+
+// This file is necessary to wrap the application in the NavProvider context
+// so that both Navbar and MobileNav components can share state.
