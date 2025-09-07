7cf3c1b820aa836b1f902be160bbe957ba579f82
midCenturyDude7 <mgriffes@gmail.com>
2025-09-06 20:51:31 -0700

    Added animated mobile nav with curve

--- app/components/Header/MobileNav/MobileNav.jsx
+++ app/components/Header/MobileNav/MobileNav.jsx
@@ -10,14 +10,14 @@ import Curve from "./Curve/Curve";
 
 export default function MobileNav({ focused, setFocused }) {
   const { selectedTab, setSelectedTab } = useNavContext();
-  const [mobileNavBar, toggleMobileNavBar] = useCycle(false, true);
+  const [mobileNavbar, toggleMobileNavbar] = useCycle(false, true);
 
   return (
     <nav className="ml-auto px-8 py-4 lg:hidden">
       <div className="relative z-10">
         <motion.button
-          animate={mobileNavBar ? "open" : "closed"}
-          onClick={() => toggleMobileNavBar()}
+          animate={mobileNavbar ? "open" : "closed"}
+          onClick={() => toggleMobileNavbar()}
           className="flex flex-col space-y-2"
         >
           {/* HAMBURGER TO X ANIMATION */}
@@ -30,24 +30,160 @@ export default function MobileNav({ focused, setFocused }) {
               },
               open: { rotate: 45, y: 8 },
             }}
-            className="block h-[0.125rem] w-8 rounded-lg bg-slate-400/75"
+            className="block h-[0.125rem] w-9 rounded-lg bg-gradient-to-r from-[#45e1e698] via-[#0084ffad] to-[#45e1e698] transition-colors duration-1000 ease-in-out"
           />
           <motion.span
             variants={{
               closed: { opacity: 1 },
               open: { opacity: 0 },
             }}
-            className="block h-[0.125rem] w-8 rounded-lg bg-slate-400/75"
+            className="block h-[0.125rem] w-9 rounded-lg bg-gradient-to-r from-[#0f2124] via-[#0084ffad] to-[#0f2124] transition-colors duration-1000 ease-in-out"
           />
           <motion.span
             variants={{
               closed: { rotate: 0, y: 0 },
               open: { rotate: -45, y: -12 },
             }}
-            className="block h-[0.125rem] w-8 rounded-lg bg-slate-400/75"
+            className="block h-[0.125rem] w-9 rounded-lg bg-gradient-to-r from-[#45e1e698] via-[#0084ffad] to-[#45e1e698] transition-colors duration-1000 ease-in-out"
           />
         </motion.button>
       </div>
+      <AnimatePresence mode="wait">
+        {mobileNavbar && (
+          <motion.div
+            key="mobile-navbar"
+            variants={menuSlide}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className="fixed inset-0 flex h-full flex-col items-center justify-center gap-10 bg-gradient-to-b from-[rgba(12,12,39,1)] to-[rgb(0,2,8)]"
+          >
+            <motion.ul
+              onPointerLeave={() => setFocused(null)}
+              className="flex flex-col justify-center"
+            >
+              {mobileNavItems.map(({ path, label, id }) => {
+                const isActive = path === selectedTab;
+                return (
+                  <li
+                    className={cn(
+                      "relative flex space-y-1 border border-x-0 border-b-slate-400/10 border-t-transparent pb-4 pt-[0.5px] last:border-none",
+                      selectedTab === path
+                        ? "bg-gradient-to-r from-[#111329a3] via-[#0321237a] to-transparent transition-colors duration-1000 ease-in-out"
+                        : "",
+                    )}
+                    key={id}
+                  >
+                    <motion.div
+                      variants={slide}
+                      initial={"initial"}
+                      animate="enter"
+                      exit="exit"
+                      className={cn(
+                        selectedTab === path
+                          ? "bg-cyan-200/50] absolute left-[-5px] top-0 z-[2] block h-full w-2 border border-[#00b7ff]"
+                          : "",
+                      )}
+                    />
+                    {focused === label ? (
+                      <motion.div
+                        transition={{
+                          layout: {
+                            duration: 0.25,
+                            ease: "easeOut",
+                            type: "spring",
+                            bounce: 0,
+                            damping: 50,
+                            mass: 0.5,
+                            stiffness: 500,
+                          },
+                        }}
+                        className={cn(
+                          // isActive
+                          //   ? "bg-cyan-200/50] absolute left-[-5px] top-0 z-[2] block h-full w-2 border border-[#00b7ff]"
+                          //   : "",
+                          "absolute left-[-5px] top-[-4px] z-[1] block h-full w-2 bg-slate-200/75 hover:border",
+                        )}
+                        layoutId="marker"
+                      />
+                    ) : null}
+                    {!focused && selectedTab === path ? (
+                      <motion.div
+                        transition={{
+                          layout: {
+                            duration: 0.25,
+                            ease: "easeOut",
+                            type: "spring",
+                            bounce: 0,
+                            damping: 50,
+                            mass: 0.5,
+                            stiffness: 400,
+                          },
+                        }}
+                        layoutId="marker"
+                        className={cn(
+                          "absolute left-[-5px] top-[-4px] z-[1] block h-full w-2 bg-slate-200/75",
+                        )}
+                      />
+                    ) : null}
+                    <Link key={id} href={path}>
+                      <motion.button
+                        data-active={isActive}
+                        onClick={() => {
+                          setSelectedTab(path);
+                          toggleMobileNavbar();
+                        }}
+                        onFocus={() => setFocused(label)}
+                        onBlur={() => setFocused(null)}
+                        onPointerEnter={() => setFocused(label)}
+                        tabIndex={0}
+                        variants={slide}
+                        initial={"initial"}
+                        animate="enter"
+                        exit="exit"
+                        className={cn(
+                          "px-4 text-4xl text-slate-400/80 transition-colors duration-1000 ease-in-out hover:text-slate-200/80",
+                          path === selectedTab
+                            ? "cursor-default text-slate-200/80"
+                            : "",
+                        )}
+                      >
+                        <span>{label}</span>
+                      </motion.button>
+                    </Link>
+                  </li>
+                );
+              })}
+            </motion.ul>
+            <motion.div className="h-px w-[95%] bg-slate-500/20" />
+            <motion.div
+              variants={slide}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              <motion.ul
+                variants={slide}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+                className="flex items-center justify-center space-x-8"
+              >
+                <li>
+                  <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
+                </li>
+                <li>
+                  <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
+                </li>
+                <li>
+                  <div className="h-10 w-10 rounded-lg bg-slate-400/80" />
+                </li>
+              </motion.ul>
+            </motion.div>
+            <Curve />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }
