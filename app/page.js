0721821fa46c1f9f9ba16a440ca108abb43eed4b
midCenturyDude7 <mgriffes@gmail.com>
2025-09-08 20:44:45 -0700

    Getting responsive design set up

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-4">
+    <div className="flex flex-col items-center justify-center space-y-4 px-6 sm:px-10 md:px-8 lg:px-12 xl:px-16">
       <h1 className="text-3xl font-bold text-slate-300/70">
         Portfolio Site — Built with Next.js, React and Framer-Motion
       </h1>
