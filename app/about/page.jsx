e326f1edb46cdb9fdc0cb9f456d2cd27f7363cc1
midCenturyDude7 <mgriffes@gmail.com>
2025-09-09 20:56:55 -0700

    Added responsive styles

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-4">
-      <h1 className="text-3xl font-bold text-slate-300/70">
+    <div className="flex flex-col items-center justify-center space-y-4 px-6 sm:px-10 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
+      <h1 className="text-3xl font-bold text-slate-300/70 sm:text-center">
         Portfolio Site — Built with Next.js, React and Framer-Motion
       </h1>
       <h2>ABOUT PAGE</h2>
