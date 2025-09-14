e717addb70a7153afc88825d63e0c271d6a32ffb
midCenturyDude7 <mgriffes@gmail.com>
2025-09-14 14:03:45 -0700

    Updated responsive break points

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex w-full flex-col px-6 sm:px-10 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
       <div className="flex min-w-full flex-wrap content-stretch items-start justify-center gap-4">
-        <div className="flex min-h-60 min-w-[90%] items-center justify-center border border-slate-300/70">
+        <div className="flex min-h-60 min-w-[90%] items-center justify-center border border-slate-300/70 md:min-w-[50%] lg:min-w-[25%]">
           <h1>1</h1>
         </div>
       </div>
