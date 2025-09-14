3f80d03deaf5293093789e7c656b61676ed1ce00
midCenturyDude7 <mgriffes@gmail.com>
2025-09-13 20:47:04 -0700

    Working with flexbox

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4 px-6 sm:px-10 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
-      <h1 className="text-3xl font-bold text-slate-300/70 sm:text-center">
-        Portfolio Site — Built with Next.js, React and Framer-Motion
-      </h1>
-      <h2>HOME PAGE</h2>
+      <div className="flex flex-wrap content-stretch justify-center gap-4">
+        <div className="min-h-20 min-w-[200px] border border-slate-300/70">
+          <h1>1</h1>
+        </div>
+      </div>
     </div>
   );
 }
