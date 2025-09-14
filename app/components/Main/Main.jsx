9b7eb15026728de191583c6cedccc2e9bb7e8f5e
midCenturyDude7 <mgriffes@gmail.com>
2025-09-13 21:14:37 -0700

    Working with flexbox and responsive design

--- app/components/Main/Main.jsx
+++ app/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 export default function Main({ children }) {
   return (
-    <div className="flex min-h-[100%] flex-1 flex-col justify-center">
+    <div className="flex min-h-[100%] flex-1 flex-col justify-start">
       {children}
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-4 px-6 sm:px-10 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
-      <div className="flex flex-wrap content-stretch justify-center gap-4">
-        <div className="min-h-20 min-w-[200px] border border-slate-300/70">
+    <div className="flex w-full flex-col px-6 sm:px-10 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
+      <div className="flex min-w-full flex-wrap content-stretch items-start justify-center gap-4">
+        <div className="flex min-h-60 min-w-[90%] items-center justify-center border border-slate-300/70">
           <h1>1</h1>
         </div>
       </div>
