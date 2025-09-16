07e809ad5ba72cf24e6189d5f5cbc107ececd62b
midCenturyDude7 <mgriffes@gmail.com>
2025-09-15 21:17:37 -0700

    Adjusting grid styles

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-[80vh] w-full items-center justify-center px-2 sm:px-6 md:h-[50vh] md:px-8 lg:px-12 xl:px-16 2xl:px-20">
-      <div className="mx-auto grid h-full w-full grid-rows-4 gap-8 md:max-w-[90%] md:grid-cols-[1fr_3fr] md:grid-rows-3 lg:max-w-[55%]">
+    <div className="flex h-[80vh] w-full items-center justify-center px-2 sm:px-6 md:h-[60vh] md:px-8 lg:px-12 xl:px-16 2xl:px-20">
+      <div className="mx-auto grid h-full w-full grid-rows-4 gap-8 md:w-[90%] md:grid-cols-[3fr_5fr] md:grid-rows-3 lg:max-w-[80%]">
         <div className="h-full w-full place-content-center border border-slate-300/20">
           1
         </div>
